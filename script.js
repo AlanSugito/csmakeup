50d0679fa7498dc285e4e1e17632a73f7298644b
Alan Sugito <alansugito75@gmail.com>
2021-11-17 17:20:36 +0700

    responsive

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 AOS.init();
+const burger = document.querySelector("#navbar .burger");
+const nav = document.querySelector("#navbar ul");
+
+burger.addEventListener("click", function(){
+    nav.classList.toggle("slide");
+    console.log(nav)
+});
